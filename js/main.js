1535bd2658a500633105cebd8f0f8954de505a7c
bhindabal <bhindabal@gmail.com>
2016-11-09 23:00:53 -0800

    Zodiac Signs

---
+++ js/main.js
@@ -0,0 +1,110 @@
+var singer= [
+	{
+		name: "Aries",
+		image: "img/aries.png",
+		about: "The Aries horoscope 2017 shows that you will be more in control of your emotions than usual during this year. But don't forget to use logic when it comes to making important decisions. The 2017 Aries horoscope predicts that during this year you are bound to have an exciting time. New opportunities are bound to come your way which will lead to you expending more energy than usual. If you take things the right way you will be able to get out of all of your adventures without more than a bruise or a scrape.",
+		keyword: "ARIES"
+	},
+	{
+		name: "Taurus",
+		image: "img/taurus.png",
+		about: "The 2017 Taurus horoscope predicts that in general, you are bound to have more energy than usual. This may feel strange and you may not know what to do with it. No matter what you do, don’t waste this energy! It won't last forever so use it when you can. Zodiac 2017 predictions for Taurus zodiac sign suggest this is the right year to impress your boss or friends. Work on your relationships or hobbies. In general, have a good time, try new things, and don't be afraid to plan for the future while to still have the energy to do it.",
+		keyword: "TAURUS"
+	},
+	{
+		name: "Gemini",
+		image: "img/gemini.png",
+		about: "The 2017 Gemini horoscope predicts that there will be slow changes during this year, which will make it easy for you to keep up with. Try to reduce your stress levels as much as possible. This will help to keep your mental health in peak health just like your physical body. Try to be as optimistic as possible during this year. The more cheerfully that you think about the future, the more likely that you are going to have a good time. The 2017 forecast for Gemini asks you to use your free time to advance as a person as well. While you can focus on work all year, you can also use your time to do the things that you love instead.",
+		keyword: "GEMINI"
+	},
+	{
+		name: "Cancer",
+		image: "img/cancer.png" ,
+		about: "The Cancer horoscope 2017 predicts that things are going to be moving quickly this year. So try to take things in stride. Don't try to do more than you know you can handle. Make sure to think before you act, not doing so could easily cause problems. The Cancer man in love can use their romantic feelings to make relationship better. In general, just use your bursts of energy productively and try to have a good year. Life can be confusing at times, but things can be a little easier when you know what to expect. With this 2017 yearly horoscope for Cancer you can be a little more sure of what your year will bring.",
+		keyword: "CANCER" ,
+	},
+	{
+		name: "Leo" ,
+		image: "img/leo.png" ,
+		about: "The Leo horoscope 2017 predicts that in general, this year will be a bit slow but this will make it easier for you. Take things in stride. With time feeling like it’s moving at a slower pace, you may also feel like you are able to get more done this year. Use your time wisely. The 2017 astrology for Leos asks you to do your best to focus on advancing your career. After your professional commitments, work on improving your relationships with friends and lovers." ,
+		keyword: "LEO" ,
+	},
+	{
+		name: "Virgo" ,
+		image: "img/virgo.png",
+		about: "The 2017 Virgo horoscope predicts that in general, your year will move at a slow pace, but advance at the same time. You may not feel like you are making as big of an impact as you would like to, but it will all pay off in the long run. The Virgo star sign 2017 forecasts warn you to make sure that you are courteous to your bosses, friends and partners. Your relationships are highly important during this time. Try not to let things stress you out too much in 2017. Always try to spare some time for the people and hobbies that you love." ,
+		keyword: "VIRGO" ,
+	},
+	{
+		name: "Libra" ,
+		image:  "img/libra.png",
+		about: "The Libra horoscope 2017 predicts that this year will go by quickly, but that doesn’t mean that you will miss out on important opportunities. You will feel like you have more energy than usual, so use this to your advantage! Try to keep your personal world balanced, but also leave some time to spend with friends, partners, and family. The yearly 2017 predictions for the Libra star sign says that you will have a great year if you play your cards right. Keep yourself balanced, and your year should be balanced as well." ,
+		keyword: "LIBRA" ,
+	},
+	{
+		name: "Scorpio" ,
+		image:  "img/scorpio.png",
+		about: "The Scorpio horoscope 2017 predicts that this year you will have your ups and downs, but remember to try to stay as optimistic as possible. The more relaxed and less stressed out you are the more likely you are to succeed. Take things one day at a time. The birthday horoscope 2017 also says that the Scorpio star sign will get a lot done this year in both career and social life. But that it doesn’t mean that you need to move quickly. Take your time; this is one way that you can stay in your comfort zone. However, the Scorpions should also try some new things this year; it is sure to make your year all the more interesting and exciting!" ,
+		keyword: "SCORPIO" ,
+	},
+	{
+		name: "Sagittarius" ,
+		image:  "img/sagittarius.png",
+		about: "The Sagittarius 2017 horoscope predictions suggest that in general, even though this year will go by quickly you need to remember to keep yourself composed as often as possible. Getting stressed out about everything will only hurt, not help. If you need a mental vacation, don’t be afraid to take one. Be kind to your friends and family. Don’t push your mind or body too far. Know your limits, but don’t put any limitations on how great your year can be if you play to your strengths." ,
+		keyword: "SAGITTARIUS" ,
+	},
+	{
+		name: "Aquarius" ,
+		image:  "img/aquarius.png",
+		about: "The Aquarius 2017 horoscope predictions suggest that this year your life will take a more serious turn than it has in recent years. While you may be bored at times, know that you can do exciting things without emptying your bank account. The sun sign 2017 forecasts suggest that the Aquarius star sign can also make friends with co-workers to make work a little less boring. Use your days off in a fun way to make your free time all the more wonderful." ,
+		keyword: "AQUARIUS" ,
+	},
+	{
+		name: "Pisces" ,
+		image:  "img/pisces.png",
+		about: "The 2017 Pisces horoscope predicts that this year will seem extremely great to you if you take all of the right steps. Sacrificing some fun for security is bound to pay off in the long run. In the meantime, don’t be afraid to have fun when you know that you can afford to do so. The Pisces star sign should never forget to spend time with friends and family when you have the chance in 2017. This will be a dynamic, and at times crazy, year for you. Have fun with it!" ,
+		keyword: "PISCES" ,
+	},
+	{
+		name: "Capricorn" ,
+		image:  "img/capricorn.png",
+		about: "The 2017 Capricorn horoscope forecasts a year where you will get time to do everything that you plan. In general, this year may seem like it is going by slowly, but this will make it easier for you to keep up with everything going on around you. Savor each day for what it is worth. The horoscope 2017 advises you to spend time with your friends and family even though you want to be focused at work. Take times to do the things that you love and you will be sure to have a good year." ,
+		keyword: "CAPRICORN" ,
+	},
+	{
+		name: " ",
+		image: "img/ ",
+		about: " ",
+		keyword: " ",
+	},
+]
+
+
+
+
+function getInfo() {
+
+	var userInput = document.getElementById('search').value
+	var user = userInput.toUpperCase()
+
+	for (i = 0; i < singer.length; i++) {
+		if (user == singer[i].keyword) {
+			var newH1 = document.createElement('h1')
+			var hello = document.getElementById('display')
+			hello.appendChild(newH1)
+			newH1.setAttribute('id', 'jhund')
+			newP = document.createElement('p')
+			newP.setAttribute('id', 'about')
+			hello.appendChild(newP)
+			var find = document.getElementById('find')
+			find.className = "gucci"
+			// var newText = document.createTextNode('hello')
+			// newH1.appendChild(newText)
+			 // document.getElementById("jhund").textContent = singer[i].name
+			 document.getElementById("about").textContent = singer[i].about
+			 document.getElementById("image").src = singer[i].image
+
+		} 
+	} 
+}
+
